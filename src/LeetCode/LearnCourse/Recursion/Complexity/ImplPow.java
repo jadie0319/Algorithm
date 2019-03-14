7347879b8a5b6af4a12ac11ef6231143897eafc1
Jaeyong <koola97620@nate.com>
2019-03-14 23:00:06 +0900

    190314 LeetCode

--- src/LeetCode/LearnCourse/Recursion/Complexity/ImplPow.java
+++ src/LeetCode/LearnCourse/Recursion/Complexity/ImplPow.java
@@ -7,7 +7,34 @@ package LeetCode.LearnCourse.Recursion.Complexity;
  */
 public class ImplPow {
   public static void main(String[] args) {
-    
+    ImplPow implPow = new ImplPow();
+
+    System.out.println(implPow.myPow(2.0,-2));
+    System.out.println(Math.pow(2.0, -2147483648));
+    System.out.println(Integer.MIN_VALUE);
+
+  }
+
+  public double myPow(double x, int n) {
+    if(n == Integer.MIN_VALUE) {
+      if(x == 1.0 || x == -1.0) {
+        return 1.0;
+      }
+      return 0.0;
+    }
+
+
+    double answer = 1;
+    int iterative = Math.abs(n);
+
+    for(int i=0; i < iterative ; i++) {
+      answer = answer * x;
+    }
+    if(n < 0) {
+      answer = 1 / answer;
+    }
+
+    return answer;
   }
 
 }
--- src/LeetCode/LearnCourse/Recursion/Complexity/Solution.java
+++ src/LeetCode/LearnCourse/Recursion/Complexity/Solution.java
@@ -16,6 +16,24 @@ class TreeNode {
 
 public class Solution {
   public static void main(String[] args) {
+    TreeNode root = new TreeNode(3);
+    TreeNode node1 = new TreeNode(9);
+    TreeNode node2 = new TreeNode(20);
+    TreeNode node3 = new TreeNode(15);
+    TreeNode node4 = new TreeNode(7);
+
+    root.left = node1;
+    root.right = node2;
+
+    node1.left = null;
+    node1.right = null;
+
+    node2.left = node3;
+    node2.right = node4;
+
+    Solution solution = new Solution();
+    System.out.println(solution.maxDepth(root));
+
 
   }
 
