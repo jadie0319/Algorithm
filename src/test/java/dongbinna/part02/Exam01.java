3c61f926877ed2441671988876200b1e44b3be50
koola97620 <koola97620@nate.com>
2022-05-18 23:11:23 +0900

    feta: 이것이 코딩테스트다.part02

---
+++ src/test/java/dongbinna/part02/Exam01.java
@@ -0,0 +1,26 @@
+package dongbinna.part02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Exam01 {
+
+    @Test
+    void case1() {
+        int money = 1260;
+        assertThat(count(money)).isEqualTo(6);
+    }
+
+    private static int count(int money) {
+        int answer = 0;
+        int[] coinTypes = {500,100,50,10};
+        for (int coin : coinTypes) {
+            int count = money / coin;
+            money = money % coin;
+            answer += count;
+        }
+
+        return answer;
+    }
+}
